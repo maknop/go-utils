88ea79855d8f19c4f2883eedd12f6219dec2d589
Matt Knop <knopmatthew93@gmail.com>
2023-10-09 23:05:34 -0600

    bootstraping tests

--- test/diff_test.go
+++ test/diff_test.go
@@ -1,7 +1,11 @@
 package test
 
-import "testing"
+var _ = Describe("Testing functionality of comparing two data structures", func() {
+	Describe("Ensure basic functionality of comparing two data structures", func() {
+		Context("", func() {
+			It("", func() {
 
-func TestHelloWorld(t *testing.T) {
-	// t.Fatal("not implemented")
-}
+			})
+		})
+	})
+})
